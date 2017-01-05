60f3c795360df95ad797e713df6f42cbc9142291
danialhr <danialharis@gmail.com>
2017-01-05 20:56:45 +0800

    almost complete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 // GameStart
 $('.start').on('click', function () {
-  $('.timer').text('5')
-  // countDown()
+  $('.timer').text('Go!')
+  countDown()
   callStartGame()
+  remove()
+})
+
+$('.reset').on('click', function () {
+  $('.timer').text('Go!')
+    // reset()
+    remove()
+  $('.reset').addClass('hide')
+  countDown()
+  callStartGame()
+  $('.DisplayEquation').removeClass('hide')
+  $('.Mainboard').removeClass('hide')
+})
+
+
+function remove(){
   $('.PlayerOneBox').removeClass('hide')
   $('.PlayerTwoBox').removeClass('hide')
   $('.oneA').removeClass('hide')
@@ -12,24 +28,27 @@ $('.start').on('click', function () {
   $('.whosTURN').removeClass('hide')
   $('.whosTURN').text("Player One's Turn")
   $('.start').addClass('hide')
-})
+}
+
 var currentPlayer = []
 var playerScore = []
 
 function callStartGame () {
   currentPlayer = 1 // player 1 or 2
   playerScore = [0, 0] // playerScore[0] is player 1, playerScore[1] is player 2
-  $('.PlayerOneScore').text(playerScore[0] * 10)
-  $('.PlayerTwoScore').text(playerScore[1] * 10)
+  // $('.PlayerOneScore').text(playerScore[0] * 10)
+  // $('.PlayerTwoScore').text(playerScore[1] * 10)
   boardRefresh()
 }
+
+var currentQuestion = 0
 
 var boardRefresh = function () {
   var gridNos = []
   var randomNosQuestion = []
 
 // CREATING AN ARRAYS OF RANDOM NUMBERS
-  var randomNos = function () { // select 9 numbers out of the possible 10, and store them inside a variable called 'store'
+  function randomNos () { // select 9 numbers out of the possible 10, and store them inside a variable called 'store'
     var store = []
     while (store.length !== 9) {
       var randomNumber = Math.ceil(Math.random() * 12)
@@ -51,14 +70,15 @@ var boardRefresh = function () {
 
   randomSelect(gridNos) // i am passing in gridNos into randomSelect function, so that it selects 2 numbers from my gridNos and store it in randomPlus
   console.log(randomNosQuestion)
-
 // THE CURRENT QUESTIOn
 
   var randomQuestion = function (x, y) {
     return x * y
   }
-  console.log(randomQuestion(randomNosQuestion[0], randomNosQuestion[1]))
+  // console.log('Correct answer: ' + randomQuestion(randomNosQuestion[0], randomNosQuestion[1]))
   $('.Result').text(randomQuestion(randomNosQuestion[0], randomNosQuestion[1]))
+
+  currentQuestion = randomQuestion(randomNosQuestion[0], randomNosQuestion[1])
 
   $('.Zero').text(gridNos[0])
   $('.One').text(gridNos[1])
@@ -70,9 +90,11 @@ var boardRefresh = function () {
   $('.Seven').text(gridNos[7])
   $('.Eight').text(gridNos[8])
 
+}
+
   var userAnswer = []
   var clickCounter = 0
-  console.log(userAnswer)
+  // console.log(userAnswer)
 
 // User Clicks
   $('.Box').click(function (event) {
@@ -80,100 +102,122 @@ var boardRefresh = function () {
       userAnswer.push($(this).text())
       $('.twoA').text(userAnswer[1] + ' = ')
     // console.log($(this).text());
-      checkAnswer()
+      console.log(userAnswer)
       gameState()
       clickCounter = 0
     }
-    if (clickCounter === 0) {
+    else if (clickCounter === 0) {
       userAnswer.push($(this).text())
       $('.oneA').text(userAnswer[0])
     // console.log($(this).text());
       clickCounter++
+      console.log(userAnswer)
     }
   })
 
-  var currentQuestion = randomQuestion(randomNosQuestion[0], randomNosQuestion[1])
 
   function checkAnswer () {
     if (userAnswer[0] * userAnswer[1] === currentQuestion) {
-       return true
-      console.log('true')
+      return true
     } else {
-       return false
-      console.log('false')
+      return false
     }
   }
 
-  var gameState = function () {
+  function gameState () {
     if (checkAnswer() === true) {
-      console.log("steady");
+      clearInterval(timer)
+      console.log('Correct Answer')
+      userAnswer = []
       boardRefresh()
-          $('.oneA').text(' _ ')
-          $('.twoA').text(' _ =')
+      countDown()
+      $('.oneA').text(' _ ')
+      $('.twoA').text(' _ =')
       if (currentPlayer === 1) {
         playerScore[0] = playerScore[0] + 1
         $('.PlayerOneScore').text(playerScore[0] * 10)
-        console.log(playerScore)
-      } else if (currentPlayer === 2) {
-        playerScore[1] + 1
+      } else if (currentPlayer !== 1) {
+          playerScore[1] = playerScore[1] + 1
         $('.PlayerTwoScore').text(playerScore[1] * 10)
-      }
+      }  console.log(playerScore)
     } else {
-      console.log("not true");
-      boardRefresh()
-      $('.oneA').text(' _ ')
-      $('.twoA').text(' _ =')
-      currentPlayer = 2
-      $('.whosTURN').text("Player's Two Turn")
+      console.log('Wrong Answer')
+      if (currentPlayer !== 1) {
+        alert('Game Over')
+        whoWins()
+        clearInterval(timer)
+      } else if (currentPlayer === 1){
+        userAnswer = []
+        boardRefresh()
+        countDown()
+        $('.oneA').text(' _ ')
+        $('.twoA').text(' _ =')
+        switchPlayer()
+      }
+
     }
   }
 
+  function switchPlayer() {
+    currentPlayer = 2
+  $('.whosTURN').text("Player's Two Turn")
+  boardRefresh()
+  countDown()
+    alert("It's Players Two Turn")
+  }
 
   function whoWins () {
-    if (playerScore[0] < playerScore[1]){
-      $('.whosTURN').text("Player Two Wins")
+    $('.PlayerOneBox').addClass('hide')
+    $('.PlayerTwoBox').addClass('hide')
+    $('.oneA').addClass('hide')
+    $('.Operations').addClass('hide')
+    $('.twoA').addClass('hide')
+    $('.timer').addClass('hide')
+    $('.DisplayEquation').addClass('hide')
+    $('.Mainboard').addClass('hide')
+    $('.reset').removeClass('hide')
+    if (playerScore[0] < playerScore[1]) {
+      $('.whosTURN').text('Player Two Wins')
     } else if (playerScore[0] === playerScore[1]) {
       $('.whosTURN').text("It's a Draw")
     } else if (playerScore[0] > playerScore[1]) {
-      $('.whosTURN').text("Player One Wins")
+      $('.whosTURN').text('Player One Wins')
     }
   }
 
+
+var timer = []
+var countDown = function () {
+  (function () {
+    var counter = 6
+    clearInterval(timer)
+    timer = setInterval(function () {
+      counter--
+      if (counter >= 0) {
+        $('.timer').text(counter)
+      }
+      if (counter === 0) {
+        clearInterval(counter)
+        if (currentPlayer === 1) {
+          alert('Sorry, Your are out of time')
+          switchPlayer()
+        } else if (currentPlayer === 2) {
+          alert('Game Over')
+          whoWins()
+        }
+      }
+    }, 1000)
+  })()
 }
 
 
-//
-//     // nextquestion
-//     } else {
-//       console.log('False')
-//       if (currentPlayer === 1) {
-//         currentPlayer = 2
-//         $('.whosTURN').text("Player Two's Turn")
-//       } else {
-//         gameOver()
-//       }
-//     }
-//   }
-// }
-
-function isGameOver () {
-  // check timer
-  // check score
-}
-
-function changeTurn () {
-
-}
-
-function gameOver () {
-  // happens when Player2 is not able to finish a question or when Player2 hits above Player1 score
-}
-
-
-
-function restart () {
-  currentPlayer = 1 // restart to Player1
-  playerScore = [0, 0] // restart to 0 for both players
+function reset(){
+  currentPlayer = 1 // player 1 or 2
+  playerScore = [0, 0] // playerScore[0] is player 1, playerScore[1] is player 2
+  $('.DisplayEquation').removeClass('hide')
+  $('.Mainboard').removeClass('hide')
+  $('.PlayerOneScore').text(0)
+  $('.PlayerTwoScore').text(0)
 }
 
 //
@@ -221,22 +265,6 @@ function restart () {
   // });
   //
   // displayCount(initial);
-
-// var countDown = function () {
-//   (function () {
-//     var counter = 5
-//     setInterval(function () {
-//       counter--
-//       if (counter >= 0) {
-//         $('.timer').text(counter)
-//       }
-//       if (counter === 0) {
-//         alert('Sorry, Your are out of time')
-//         clearInterval(counter)
-//       }
-//     }, 1000)
-//   })()
-// }
 
   // if it's player1 turn chnage to player 2
   // if it's player 2 end game check score results/ game end since player2 dun meet the condition to win
